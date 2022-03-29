1eb147d3554f0de67c62289be9b98e41236b0084
Alexander Kuryatnikov <asander1995@gmail.com>
2022-03-29 15:47:59 +0300

    added get_user_model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+User = get_user_model()
 
-class User(AbstractUser):
+
+class CustomUser(AbstractUser):
     USER_ROLES = (
         ('user', 'User'),
         ('moderator', 'Moderator'),
