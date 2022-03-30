b43267ff7e0755a41ff49788ea01011caf7596f2
Артем Лайченков <frost-mail.ru@mail.ru>
2022-03-31 00:44:16 +0400

    31.03.2022 модель  Comment

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,10 +4,9 @@ from reviews.models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    # todo подумаю и допишу
-    # author = serializers.SlugRelatedField(
-        #read_only=True, slug_field='username'
-    #)
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
     class Meta:
         fields = ('id', 'text', 'author', 'score', 'pub_date')
         model = Review
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -4,10 +4,13 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('api.urls')),
+    path('api/', include('api.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
+    # JWT будут вместо auth/users как в теории api/vi/users и так далее
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
 ]
