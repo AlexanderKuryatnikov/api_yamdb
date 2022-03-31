a235b3b3075dd13465d7e5c8ec54fd70ed4b9cd3
Артем Лайченков <frost-mail.ru@mail.ru>
2022-04-01 00:25:04 +0400

    Comment viewSet_serialiser and permissions

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,16 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 
-from .serializers import CategorySerializer
-from reviews.models import Category
+from .permissions import AuthorOrReadOnly
+from .serializers import (
+    CategorySerializer, 
+    ReviewSerializer, 
+    CommentsSerializer
+)
+from reviews.models import Category, Title, Comments, Review
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -13,3 +20,28 @@ class CategoryViewSet(viewsets.ModelViewSet):
     filter_backends = (SearchFilter, )
     search_fields = ('name', )
 
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return title.reviews.all()
+    
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        serializer.save(author=self.request.user, title_id=title_id)
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentsSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_queryset(self):
+        reveiw_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=reveiw_id)
+        return review.comments.all()
