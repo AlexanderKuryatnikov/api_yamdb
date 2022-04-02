d3ec50f7926f2bfb451b5114c9438b3fe1e984e9
Артем Лайченков <frost-mail.ru@mail.ru>
2022-04-03 02:25:56 +0400

    UniqueConstraint and validators

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,45 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.validators import UniqueTogetherValidator
-from reviews.models import Review, Category, Comments
+
+from reviews.models import Review, Category, Comments, User
+
+
+class ConfirmationCodeObtainSerializer(serializers.ModelSerializer):
+    confirmation_code = serializers.HiddenField(default='')
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'confirmation_code')
+
+
+class AccessTokenObtainSerializer(TokenObtainPairSerializer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields[self.username_field] = serializers.CharField()
+        self.fields['confirmation_code'] = serializers.CharField()
+        self.fields['password'].required = False
+
+    def validate(self, data):
+        user = User.objects.filter(username=data.get('username')).first()
+        if user is None:
+            raise serializers.ValidationError(
+                'User not found'
+            )
+        if user.confirmation_code != data.get('confirmation_code'):
+            raise serializers.ValidationError(
+                'Invalid confirmation code'
+            )
+        token = self.get_token(user).access_token
+        return{'token': str(token)}
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
 
 
 class ReviewSerializer(serializers.ModelSerializer):
