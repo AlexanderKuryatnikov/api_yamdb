5e2111f76810c0bb3b21989d7648657b5f1f1bb0
Артем Лайченков <frost-mail.ru@mail.ru>
2022-04-07 20:28:37 +0400

    fixed HiddenField Review, Comments ViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -155,10 +155,12 @@ class CommentsViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, pk=review_id)
+        review = get_object_or_404(
+            Review, pk=review_id, title__id=self.kwargs.get('title_id'))
         return review.comments.all()
 
     def perform_create(self, serializer):
         review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, pk=review_id)
+        review = get_object_or_404(
+            Review, pk=review_id, title__id=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, review_id=review)
